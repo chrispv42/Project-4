0091777d4737116ad4626696f88d5f439be1ccea
chrispv42 <chrispv42@gmail.com>
2026-02-10 09:36:56 -0700

    chore: convert client from gitlink to normal folder

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is going to be Ol' Time Muscle, a pure Muscle Car site to show off cool rides in your area
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
